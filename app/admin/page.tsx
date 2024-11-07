80cbc754c814b0f2be58bdc2b5757688df1eee74
shenkzjay <senksjay@gmail.com>
2024-11-07 23:11:36 +0100

    removed login form instance from admin homepage

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,9 +2,5 @@ import LoginForm from "./auth/login/page";
 export const dynamic = "force-dynamic";
 
 export default function AdminHome() {
-  return (
-    <section>
-      <LoginForm />
-    </section>
-  );
+  return <section>{/* <LoginForm /> */}</section>;
 }
